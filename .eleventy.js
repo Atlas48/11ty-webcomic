50dc24eb42415310ad35fec9a5628774f6d087dd
Atlas <AtlasJan@gmx.com>
2020-11-23 00:21:22 +0000

    Generic work commit

--- .eleventy.js
+++ .eleventy.js
@@ -4,41 +4,28 @@ const path=require('path');
 const ast=require('ampelstatus');
 const CONFIG=__dirname+"/conf.toml";
 
-include = {
-  inc: x => x+1,
-  dec: x => x-1
-}
 module.exports=main;
 
 if(process.main!==module) {
-  console.err('This script is to not be called directly.');
+  console.err(ast.err+' This script is to not be called directly.');
   process.exit(1);
 }
 
 async function main(conf) {
+  data=new Object;
   conf.addDataExtention('toml', toml.parse);
-  //BEGIN CONFIG-PARSE
-  try {
-    var data=toml.parse(fs.readFile(CONFIG, 'utf-8'));
-  } catch(e) {
-    console.err(`11ty-webcomic: Unable to load config: ${e.message}`);
-    if(typeof data == 'undefined') var data=new Object;
-  }
-  finallly {
-    data=include.concat(data);
-  }
-  //END CONFIG-PARSE
-  //BEGIN EXTENSION-MAP
-  try {
-    data.ext=new Array;
-    for(j in await fs.readdir('./img')) {
-      let i=j.split('.');
-      data.ext[i[0]]=i[1];
+  if(typeof pug != "undefined") conf.setPugOptions(pug);
+  conf.addPlugin(require('eleventy-plugin-json-feed'), jsonfeed)
+    //BEGIN EXTENSION-MAP
+    try {
+      data.ext=new Array;
+      for await(j of fs.readdir('./img')) {
+        let i=j.split('.');
+        data.ext[i[0]]=i[1];
+      }
+    } catch {
+      console.err(`${ast.err} 11ty-webcomic: Unable to create file extension map: ${e.message}`)
     }
-  } catch {
-    console.err(`11ty-webcomic: Unable to create file extension map: ${e.message}`)
-  }
-  //END EXTENSION-MAP
-  if(typeof data.pug == 'object') conf.setPugOptions(data.pug);
+    //END EXTENSION-MAP
   return data;
 }
---
+++ _data/f.js
@@ -0,0 +1,4 @@
+module.exports = {
+    inc: x => x+1,
+    dec: x => x-1
+}
